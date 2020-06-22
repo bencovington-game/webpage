6ecdebe060a196973730cdcfdf6962e4fcc308bb
ashleycrouch <ashley.crouch@rocketmail.com>
2020-06-22 12:14:35 -0500

    Updates

---
+++ precache-manifest.12cd2804b2246625eae38bbbf9f97853.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc2329fd37929d6322af143110ad661",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "f04de0219605c28ce920",
+    "url": "/webpage/static/css/2.68079c1c.chunk.css"
+  },
+  {
+    "revision": "e5781a053c54973b1a42",
+    "url": "/webpage/static/css/main.7a09124b.chunk.css"
+  },
+  {
+    "revision": "f04de0219605c28ce920",
+    "url": "/webpage/static/js/2.f3b2ad11.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/webpage/static/js/2.f3b2ad11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5781a053c54973b1a42",
+    "url": "/webpage/static/js/main.afe80da5.chunk.js"
+  },
+  {
+    "revision": "fb70f944525608707990",
+    "url": "/webpage/static/js/runtime-main.c7a8547b.js"
+  },
+  {
+    "revision": "59a4e8d6615525be9d50d64446b01ca7",
+    "url": "/webpage/static/media/Monstralia.59a4e8d6.png"
+  },
+  {
+    "revision": "88f5c2813ad3a389e37a237a0a9c78e8",
+    "url": "/webpage/static/media/background.88f5c281.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/webpage/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/webpage/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/webpage/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/webpage/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/webpage/static/media/slick.f97e3bbf.svg"
+  }
+]);
